84d3894f9528457edc49c5d1b50e4a3a0d9b6fa7
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-01-04 21:55:14 +0530

    Create arithmetic-operation-with-int.cpp

---
+++ basics/arithmetic-operation-with-int.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1=12;  //int variable declaration
+    int num2=6;
+    
+    int add_result=num1+num2; //adds both numbers
+    int subtract_result=num1-num2; //subtracts both numbers
+    int multiply_result=num1*num2; //multiplies both numbers
+    int divide_result=num1/num2; //quotient of both numbers
+    int modulo_result=num1%num2; //remainder of both numbers
+    
+    cout<< add_result <<endl;
+    cout<< subtract_result <<endl;
+    cout<< multiply_result <<endl;
+    cout<< divide_result <<endl;
+    cout<< modulo_result <<endl;
+    
+    return 0;
+}
