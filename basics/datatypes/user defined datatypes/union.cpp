c72954aea28ed58c3d34eda8534f97ed71384bbf
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-01-12 15:03:50 +0530

    Create union.cpp

--- basics/datatypes/user defined datatypes/union.cpp
+++ basics/datatypes/user defined datatypes/union.cpp
@@ -1,1 +1,25 @@
-A union in C++ is a data structure that allows different data types to be stored in the same memory location.
+//A union in C++ is a data structure that allows different data types to be stored in the same memory location.
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+//Creating a union using union keyword
+union A{
+    int a;
+    char b;
+};
+
+int main()
+{
+    A t; //Instance for the union
+    
+    t.b = 'B'; //Only char is assigned value
+    
+    cout << "int a:" << t.a << endl; //int a:66
+    cout << "char b:" << t.b << endl; //char b:B
+    return 0;
+}
+
+//Note that int a has not been assigned any value but still it has been printed
+//This is because both int and char are stored in the same memory and hence the equivalent int value of B is printed
