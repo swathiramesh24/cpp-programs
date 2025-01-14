97c5e10df44916a2d00f2cc3d080c350686ce69d
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-01-14 15:04:51 +0530

    Create kitty_message.cpp

---
+++ fun programs/kitty_message.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "|￣￣￣￣￣￣￣|" << endl;
+    cout << "|   MESSAGE   |" << endl;
+    cout << "|    HERE     |" << endl;
+    cout << "|_____________|" << endl;
+    cout << "(\\__/)" << " ||" << endl;
+    cout << "(•ㅅ•)" << " ||" << endl;
+    cout << "/ 　 づ" << "  ||" << endl;
+    return 0;
+}
