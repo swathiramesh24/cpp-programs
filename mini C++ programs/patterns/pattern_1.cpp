dce72e7c94f36d7ad448b7101f044a989ba01087
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-02-03 20:19:08 +0530

    Create pattern_1.cpp

---
+++ mini C++ programs/patterns/pattern_1.cpp
@@ -0,0 +1,24 @@
+/*
+
+*
+**
+***
+****
+*****
+
+*/
+
+#include<iostream>
+
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            std::cout << "*";
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
